ea08cfa231132508aa4913f955380d9ab567a2d1
RenzoGiovani <PT77564074@idat.edu.pe>
2023-03-03 18:49:08 -0500

    subir proyecrto

---
+++ src/main/java/com/idat/edu/pe/LoginViernesApplication.java
@@ -0,0 +1,13 @@
+package com.idat.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginViernesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginViernesApplication.class, args);
+	}
+
+}
